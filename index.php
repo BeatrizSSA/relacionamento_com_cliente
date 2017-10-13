d4e6aa17d1e0673232dd6abb691afbc172e2a449
Juliana Maria <jullyannalovely@gmail.com>
2017-10-13 14:05:29 -0300

    implemetanção do carousel no index

--- index.php
+++ index.php
@@ -7,9 +7,16 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta charset="utf-8">
   <title>Home</title>
-  <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
-  <script type="text/javascript" src="bootstrap/js/jquery-min.js"></script>
-  <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style>
+  .carousel-inner > .item > img,
+  .carousel-inner > .item > a > img {
+      width: 70%;
+      margin: auto;
+  }
+  </style>
 </head>
 <body>
 
@@ -22,41 +29,65 @@
   proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
   </p>
 
-
-  <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+<div class="container">
+  <br>
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
     <!-- Indicators -->
     <ol class="carousel-indicators">
-      <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-      <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-      <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+      <li data-target="#myCarousel" data-slide-to="3"></li>
     </ol>
 
     <!-- Wrapper for slides -->
     <div class="carousel-inner" role="listbox">
+
       <div class="item active">
-        <img src="hulk.jpg" alt="...">
+        <img src="teste1.jpg" alt="Chania" width="460" height="345">
         <div class="carousel-caption">
-          ...
+          <h3>Lorem ipsum</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore laboriosam beatae ducimus iste minus a error necessitatibus adipisci voluptate, harum eligendi, qui nemo mollitia. Magnam cumque, similique vitae corporis sit!</p>
         </div>
       </div>
+
       <div class="item">
-        <img src="gato.jpg" alt="...">
+        <img src="teste2.jpg" alt="Chania" width="460" height="345">
         <div class="carousel-caption">
-          ...
+          <h3>Lorem ipsum</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque consequuntur reiciendis aliquid iure, fugit animi dicta ut dolore dolorem repellat? Cupiditate voluptas, itaque a veritatis, consequuntur sunt porro distinctio saepe.</p>
         </div>
       </div>
-      ...
+    
+      <div class="item">
+        <img src="teste3.jpg" alt="Flower" width="460" height="345">
+        <div class="carousel-caption">
+          <h3>Lorem ipsum</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque saepe ipsa blanditiis culpa commodi minima quia repudiandae placeat doloribus ducimus reiciendis, soluta magni, quis explicabo, laboriosam numquam ab assumenda reprehenderit. in Kolymbari, Crete.</p>
+        </div>
+      </div>
+
+      <div class="item">
+        <img src="teste4.jpg" alt="Flower" width="460" height="345">
+        <div class="carousel-caption">
+          <h3>Lorem ipsum</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi quis est dolores, perferendis tempore a error, qui non libero veritatis eaque laboriosam fugiat nam nisi, praesentium porro nihil consequatur et! in Kolymbari, Crete.</p>
+        </div>
+      </div>
+  
     </div>
 
-    <!-- Controls -->
-    <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
       <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
       <span class="sr-only">Previous</span>
     </a>
-    <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
       <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
       <span class="sr-only">Next</span>
     </a>
   </div>
+</div>
+
 </body>
 </html>
