7e16502eea3c923c3eb5f767ae044828055f5e9e
Juliana Maria <jullyannalovely@gmail.com>
2017-10-27 14:35:07 -0300

    add footer

---
+++ footer.php
@@ -0,0 +1,27 @@
+ 
+ 
+ <div class="push"></div>
+    </div>
+<footer class="footer">
+	<style type="text/css">
+	footer {
+		background-color: black;
+		color: white;
+	}
+    .footer,
+    .push {
+      height: 50px;
+    }
+	</style>
+		<form class="form-horizontal">
+  			<div class="form-group">
+   			<label class="control-label col-sm-2">Um serviço produzido por WebMarket dúvidas? Ligue (81)3555-4667</label>
+    			<div class="col-sm-5">
+    				  <div class="input-group">
+ 					 </div>
+   				</div>
+  			</div>
+		</form>
+	</footer> 
+</body>
+</html>
