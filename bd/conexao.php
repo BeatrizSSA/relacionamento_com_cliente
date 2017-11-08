0bd165a2161a0ab9bdadc3cb8bd9f0af528b7090
BeatrizSSA <andradebeatriz626@gmail.com>
2017-11-07 23:03:16 -0300

    QUERIDA E AMADA BD, ainda -_-

--- bd/conexao.php
+++ bd/conexao.php
@@ -1,40 +1,15 @@
 <?php 
- /*  
-  * Constantes de parâmetros para configuração da conexão  
-  */  
- define('HOST', 'localhost:13306');  
- define('DBNAME', 'id2969958_cadastro');  
- define('CHARSET', 'utf8');  
- define('USER', 'root');  
- define('PASSWORD', '123456');  
+  $dbname = "id2969958_cadastro";
+  $usuario="id2969958_beatriz";
+  $senha = "123456";
+  
+try {
+      $conn = new PDO("mysql:host=localhost;dbname=$dbname", $usuario, $senha);
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  } catch(PDOException $e) {
+      echo 'ERROR: ' . $e->getMessage();
+  }
 
- class Conexao {  
 
-   /*  
-    * Atributo estático para instância do PDO  
-    */  
-   private static $pdo;
 
-   /*  
-    * Escondendo o construtor da classe  
-    */ 
-   private function __construct() {  
-     //  
-   } 
- 
-   /*  
-    * Método estático para retornar uma conexão válida  
-    * Verifica se já existe uma instância da conexão, caso não, configura uma nova conexão  
-    */  
-   public static function getInstance() {  
-     if (!isset(self::$pdo)) {  
-       try {  
-         $opcoes = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8', PDO::ATTR_PERSISTENT => TRUE);  
-         self::$pdo = new PDO("mysql:host=" . HOST . "; dbname=" . DBNAME . "; charset=" . CHARSET . ";", USER, PASSWORD, $opcoes);  
-       } catch (PDOException $e) {  
-         print "Erro: " . $e->getMessage();  
-       }  
-     }  
-     return self::$pdo;  
-   }  
- }
+ ?>
