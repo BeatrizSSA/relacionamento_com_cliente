749dbab62add5023208981ac80d502ff0c4ef9b8
BeatrizSSA <andradebeatriz626@gmail.com>
2017-11-17 10:31:50 -0300

    emoção momentânea! Erro na página_do_cliente.php resolvido, mas com erro de novo.

--- header.php
+++ header.php
@@ -63,13 +63,13 @@ session_start();
         $senha = "rogerinho";
         $bd = "id2969958_projeto";
         $strcon = mysqli_connect("$host","$usuario","$senha","$bd") or die('Erro ao conectar ao banco!');
-        $sql = "SELECT * FROM usuario WHERE USER_LOGIN = '$login' and USER_SENHA = '$senha'";
+        $sql = "SELECT * FROM usuarios WHERE USER_LOGIN = '$login' and USER_SENHA = '$senha'";
         $resultado = mysqli_query($strcon, $sql) or die('Erro ao tentar cadastrar registro');
-        $name = mysqli_query($strcon, "SELECT USER_NOME FROM usuario WHERE USER_LOGIN = '$login' and USER_SENHA = '$senha'") or die(mysqli_error($strcon));
+        $name = mysqli_query($strcon, "SELECT USER_NOME FROM usuarios WHERE USER_LOGIN = '$login' and USER_SENHA = '$senha'") or die(mysqli_error($strcon));
         $re = mysqli_fetch_array($name);
         echo "<li><div class='dropdown'>
               <div class='dropdown-content'>
-              <li><a href='logout.php'>Bem Vindo $llogin 
+              <li><a href='logout.php'>Bem Vindo $login 
                 Sair <i class='fa fa-power-off' aria-hidden='true'></i></a></li>
               </div>
               </div></li>";
@@ -97,9 +97,11 @@ session_start();
         <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
         <li><a href="creators.php"><span class="glyphicon glyphicon-earphone"></span> Contato</a></li>
       </ul>
-        </div>
-        </div>
-        </nav>
+      </div>
+    </nav>
+    </div>
+      
+        
       <!-- </div> -->
     <!-- </div> -->
   </header> 
--- pagina_do_cliente.php
+++ pagina_do_cliente.php
@@ -1,32 +1,35 @@
 <?php
 include "header.php";
-require "bd/conexao.php";
+require "bd/conexao.php"; 
 // Recebe o termo de pesquisa se existir
-// $termo = (isset($_GET['termo'])) ? $_GET['termo'] : '';
+$termo = (isset($_GET['termo'])) ? $_GET['termo'] : '';
  
-// // // Verifica se o termo de pesquisa está vazio, se estiver executa uma consulta completa
-//     if (empty($termo)):
+// // Verifica se o termo de pesquisa está vazio, se estiver executa uma consulta completa
+    if (empty($termo)):
      
-//         // $conexao = Conexao::getInstance();
-//         $sql = 'SELECT id, codigo, nome, quantidade, preco FROM cadastro';
-//         // $stm= $conexao->prepare($sql);
-//              /*$stm->execute();*/
-//              // $clientes = $stm->fetchAll(PDO::FETCH_OBJ);
+        // $conexao = Conexao::getInstance();
+        $sql = 'SELECT id, nome, endereco, cpf, telefone, email, login, senha FROM usuarios';
+        // $stm= $conexao->prepare($sql);
+             /*$stm->execute();*/
+             // $clientes = $stm->fetchAll(PDO::FETCH_OBJ);
           
-//     else:
+    else:
           
-//              // Executa uma consulta baseada no termo de pesquisa passado como parâmetro
-//              // $conexao = conexao::getInstance();
-//         $sql = 'SELECT id, codigo, nome, quantidade, preco FROM cadastro WHERE nome LIKE :nome OR codigo LIKE :codigo :quantidade :preco';
-//         $stm = $conexao->prepare($sql);
-//         $stm->bindValue(':nome', $termo.'%');
-//         $stm->bindValue(':codigo', $termo.'%');
-//         $stm->bindValue(':quantidade', $termo.'%');
-//         $stm->bindValue(':preco', $termo.'%');
-//         $stm->execute();
-//         $clientes = $stm->fetchAll(PDO::FETCH_OBJ);
+             // Executa uma consulta baseada no termo de pesquisa passado como parâmetro
+             // $conexao = conexao::getInstance();
+        $sql = 'SELECT id, nome, endereco, cpf, telefone, email, login, senha FROM usuarios WHERE nome LIKE :nome OR endereco LIKE :endereco :cpf :telefone';
+        $stm = $conexao->prepare($sql);
+        $stm->bindValue(':nome', $termo.'%');
+        $stm->bindValue(':endereco', $termo.'%');
+        $stm->bindValue(':cpf', $termo.'%');
+        $stm->bindValue(':telefone', $termo.'%');
+        $stm->bindValue(':email', $termo.'%');
+        $stm->bindValue(':login', $termo.'%');
+        $stm->bindValue(':senha', $termo.'%');
+        $stm->execute();
+        $clientes = $stm->fetchAll(PDO::FETCH_OBJ);
          
-//     endif;
+    endif;
 
 ?>
 
@@ -34,7 +37,7 @@ require "bd/conexao.php";
  <br>
  <br>
 
-    <form action="" method="get" id='form-contato' class="form-horizontal col-md-10">
+   <!--  <form action="" method="get" id='form-contato' class="form-horizontal col-md-10">
         <label class="col-md-2 control-label" for="termo">Pesquisar</label>
             <div class='col-md-7'>
                 <input type="text" class="form-control" id="termo" name="termo" placeholder="Infome o Nome do produto">
@@ -44,26 +47,28 @@ require "bd/conexao.php";
     </form>    
                 
     <a href='lista1.php' class="btn btn-success pull-right">Cadastrar Novo Produtos</a>
-    <div class='clearfix'></div> 
+    <div class='clearfix'></div>  -->
 
 
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html>
     <head>
         <link rel="stylesheet" type="text/css" href="itens.css">
         <link rel="stylesheet"  href="./font-awesome-4.7.0/css/font-awesome.min.css">
 
-        <title>Meus Produtos</title>
+        <title>Usuários Cadastrados</title>
     </head>
     
-    <body>
+    <body> -->
         <br>
         <br>
         <table class="quadrado">
             <tr>
-                <th>nome </th>
-                <th>quantidade</th>
-                <th>preco</th>
+                <th>Nome </th>
+                <th>Endereço</th>
+                <th>CPF</th>
+                <th>Telefone</th>
+                <th>Email</th>
                 <th>Deletar</th>
                 <th>editar</th>
             </tr>
@@ -75,9 +80,9 @@ require "bd/conexao.php";
         $senha = "rogerinho";
         $bd = "id2969958_projeto";
         $strcon = mysqli_connect("$host","$usuario","$senha","$bd") or die('Erro ao conectar ao banco!');
-        $sql = "SELECT * FROM cadastro";
+        $sql = "SELECT * FROM usuarios";
         $resultado = mysqli_query($strcon, $sql) or die('Erro ao tentar cadastrar registro');
-        $name = mysqli_query($strcon, "SELECT id, nome, quantidade, preco  FROM cadastro") or die(mysqli_error($strcon));
+        $name = mysqli_query($strcon, "SELECT id, nome, endereco, cpf, telefone, email, login, senha  FROM usuarios") or die(mysqli_error($strcon));
         $re = mysqli_fetch_array($name);
         while($dados = mysqli_fetch_array($resultado)): 
     ?>
@@ -86,23 +91,32 @@ require "bd/conexao.php";
 
         $id = $dados['id'];
         $nome=$dados['nome'];
-        $quantidade = $dados['quantidade'];
-        $preco = $dados['preco'];
+        $endereco = $dados['endereco'];
+        $cpf = $dados['cpf'];
+        $telefone = $dados['telefone'];
+        $email = $dados['email'];
+        $login = $dados['login'];
+        $senha = $dados['senha'];
         echo "<tr>
-                <td> ".$nome."</td>
-                <td>".$quantidade."</td>
-                <td>".$preco."</td>
+                <td>".$nome."</td>
+                <td>".$endereco."</td>
+                <td>".$cpf."</td>
+                <td>".$telefone."</td>
+                <td>".$email."</td>
+                <td>".$login."</td>
+                <td>".$senha."</td>
                 <td><a href='bd/excluir.php?id=".$id."'><i class='fa fa-2x fa-trash sr-icons' title='Deletar Produto'></i></a></td>
                 <td><a href='bd/editar.php?id=".$row['id']."'><i class='glyphicon glyphicon-cog' title='editar Produto'></i></a></td>
             </tr>";
     ?>
       
-        <?php
+    <?php
 
-            endwhile;
-            mysqli_close($strcon);
+        endwhile;
+        mysqli_close($strcon);
 
-        ?>
+    ?>
+
     </table>
     <br>
 
