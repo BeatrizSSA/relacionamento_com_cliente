9edcfd824d4095716b8518f92646828dced75861
BeatrizSSA <beatrizandrade626@gmail.com>
2017-12-05 22:03:29 -0300

    pesquisar.php funcional

--- pesquisar.php
+++ pesquisar.php
@@ -13,6 +13,29 @@
     <?php
     $prat = $_POST['pesquisa'];
     echo "<h1>Pesquisando por ".$prat."... </h1>";
+
+    // if (isset($_POST['pesquisa']) == true) {
+    //   echo "<table>";
+
+    //   echo" <tr>
+    //           <th>Nome </th>
+    //           <th>Quantidade</th>
+    //           <th>Preco</th>
+    //         </tr>";
+    //   foreach($prat as $row) {
+    //     echo "
+    //           <tr>
+    //             <td>".$row['nome']."</td>
+    //             <td>".$row['quantidade']."</td>
+    //             <td>".$row['preco']." </td>
+    //           </tr>
+    //           ";
+    //   }
+    //   echo "</table>";
+    // }else{
+    //   echo "<h1>Não tem no estoque ".$prat."... </h1>";
+    // }
+
     include 'footer.php';
     ?>
   </body>
